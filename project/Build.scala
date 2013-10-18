5222d5da58a9e7310e60e9c7bff155c9eefacda4
Patric Fornasier <patric.fornasier@gmail.com>
2013-10-18 16:11:07 +0100

    pat: working towards fixing test packaging

--- project/Build.scala
+++ project/Build.scala
@@ -1,12 +1,19 @@
+import _root_.sbt.IO
+import java.lang.management.ManagementFactory
+import java.lang.System._
+import java.net.URL
 import sbt._
 import sbt.Configuration
 import sbt.Keys._
 import scala._
-import scala.Predef._
+import scala.collection.JavaConversions._
 
 object BuildSettings {
   import Project._
   import Defaults._
+
+  val buildVersion = Option(System.getenv("GO_PIPELINE_LABEL")).getOrElse("LOCAL")
+  val debugging: Boolean = ManagementFactory.getRuntimeMXBean().getInputArguments().exists(_ == "-Xdebug")
 
   lazy val buildSettings = {
     Seq(
@@ -15,7 +22,7 @@ object BuildSettings {
     ) ++ sharedSettings ++ unitTestSettings ++ integrationTestSettings ++ functionalTestSettings
   }
 
-  lazy val Shared = config("shared") extend(Runtime)
+  lazy val Shared = config("shared") extend(Runtime) // FIXME could we rename this to test?
   lazy val sharedSettings = createTestSettings("shared", Shared, Defaults.configSettings)
 
   lazy val UnitTests = config("unit") extend(Shared)
@@ -26,9 +33,13 @@ object BuildSettings {
 
   lazy val FunctionalTests = config("functional") extend(Shared)
   lazy val functionalTestSettings = createTestSettings("functional", FunctionalTests)
+  
+  lazy val SystemTests = config("system") extend(Shared)
+  lazy val systemTestSettings = createTestSettings("system", SystemTests)  
 
   private def createTestSettings(testType: String, testConfiguration: Configuration, settings: Seq[Setting[_]] = Defaults.testSettings) = {
     inConfig(testConfiguration)(settings) ++
+    ((fork in testConfiguration) := !debugging) ++    
     (sourceDirectory in testConfiguration <<= sourceDirectory in Test) ++
     (classDirectory in testConfiguration <<= classDirectory in Test) ++    
     (sources in testConfiguration ~= { _ filter { shouldInclude(_, testType) } } ) ++
@@ -38,6 +49,17 @@ object BuildSettings {
   private def shouldInclude(testFile: File, testType: String) = {
     testFile.toURI.toString.contains("/" + testType + "/")
   }  
+  
+  private def createPackageSettings(testType: String, testConfiguration: Configuration, packagingTaskKey: TaskKey[sbt.File]) = {
+    inConfig(testConfiguration)(Defaults.testSettings) ++
+     Defaults.packageTaskSettings(packagingTaskKey, mappings in (Test, packageBin)) ++
+      (artifactName in packagingTaskKey := {(scalaversion, moduleId, artifact) => "%s_%s-%s-%s.jar" format (moduleId.name, scalaversion.binary, buildVersion, testType)}) ++
+      (mappings in packagingTaskKey ~= { (ms: Seq[(File,String)]) =>
+        ms.filter { case (file, path) =>{ file.getName.endsWith(".class")==false || file.getAbsolutePath.contains(testType) }
+        }
+      })
+  }
+  
 }
 
 object CasperBuild extends Build {
@@ -45,10 +67,11 @@ object CasperBuild extends Build {
   import BuildSettings._
 
   lazy val core = Project("core", file("."))  
-    .configs(Shared)  
+    .configs(Shared)
     .configs(UnitTests)
     .configs(IntegrationTests)
-    .configs(FunctionalTests)        
+    .configs(FunctionalTests)
+    .configs(SystemTests)
     .settings(buildSettings : _*)
 }
 
