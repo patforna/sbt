2cea38220e64060354f841756344c096a9b5d903
Patric Fornasier <patric.fornasier@gmail.com>
2013-10-20 11:43:26 +0100

    pat: rewrote zip-tests task.

--- project/Build.scala
+++ project/Build.scala
@@ -19,7 +19,9 @@ object BuildSettings {
     Seq(
       scalaVersion := "2.10.2",
       libraryDependencies := Seq("org.scalatest" % "scalatest_2.10" % "1.9.2" % "unit, integration, functional, shared")      
-    ) ++ sharedSettings ++ unitTestSettings ++ integrationTestSettings ++ functionalTestSettings ++ systemTestSettings
+    ) ++ 
+    sharedSettings ++ unitTestSettings ++ integrationTestSettings ++ functionalTestSettings ++ systemTestSettings ++
+    zipSettings
   }
 
   lazy val Shared = config("shared") extend(Runtime) // FIXME could we rename this to test?
@@ -51,6 +53,34 @@ object BuildSettings {
     testFile.toURI.toString.contains("/" + testType + "/")
   }  
   
+  
+  // --- zip task
+  lazy val zipSettings = (zip <<= zipTask) ++ (zipTests <<= zipTestsTask)
+  
+  lazy val zip = TaskKey[Unit]("zip", "Creates a deployable artifact.")
+  lazy val zipTests = TaskKey[Unit]("zip-tests", "Creates an artifact which contains integration, functional and system tests.")  
+  
+  def zipTask = {
+    (fullClasspath in Runtime, Keys.packageBin in Compile, streams) map {
+      (classpath, coreJar, streams) =>
+        val libs = (classpath.files +++ coreJar).get x flatRebase("lib")
+
+        val zipPath = coreJar.getAbsolutePath.replace(".jar", ".zip")
+        sbt.IO.zip(libs, file(zipPath))
+        streams.log.info("Created zip: " + zipPath)
+    }
+  }
+  
+  def zipTestsTask = {
+    (fullClasspath in Runtime, Keys.packageBin in Compile, Keys.packageBin in Shared, Keys.packageBin in IntegrationTests, Keys.packageBin in FunctionalTests, Keys.packageBin in SystemTests, streams) map {
+      (classpath, coreJar, sharedJar, integrationJar, functionalJar, systemJar, streams) =>
+        val libs = (classpath.files +++ coreJar +++ sharedJar +++ integrationJar +++ functionalJar +++ systemJar).get x flatRebase("lib")
+
+        val zipPath = coreJar.getAbsolutePath.replace(".jar", "-tests.zip")
+        sbt.IO.zip(libs, file(zipPath))
+        streams.log.info("Created zip: " + zipPath)
+    }
+  }  
 }
 
 object CasperBuild extends Build {
