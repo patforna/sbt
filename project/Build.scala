f23bac97bf1752e085cd3c37bc69da00360993f4
Patric Fornasier <patric.fornasier@gmail.com>
2013-10-20 11:07:40 +0100

    pat: reverting previous commit and only fixing x:packaging for now.

--- project/Build.scala
+++ project/Build.scala
@@ -16,60 +16,10 @@ object BuildSettings {
   val debugging: Boolean = ManagementFactory.getRuntimeMXBean().getInputArguments().exists(_ == "-Xdebug")
 
   lazy val buildSettings = {
-    
-    val zipDependencies = Seq(
-      packageBin in Compile
-    )    
-    
-    val testZipDependencies = Seq(      
-      // packageBin in Compile,
-      // Keys.`package` in Shared//,
-      // packageBin in IntegrationTests,
-      // packageBin in FunctionalTests,
-      // packageBin in SystemTests
-    )    
-    
-    def zipTaskSettings(key:TaskKey[Unit], task:Initialize[sbt.Task[Unit]], dependencies: Seq[ScopedTask[_]], artifactSuffix: String = "") = {
-      (key <<= task) ++
-        (artifact in key <<= name(n => Artifact(n + artifactSuffix, "zip", "zip"))) ++
-        (artifactPath in key <<= artifactPathSetting(artifact in key)) ++
-        (key <<= key.dependsOn(packageBin in Shared))
-        // dependencies.map {(key <<= (key).dependsOn(_))}
-    }
-    
-    // --- zip task
-    lazy val zip = TaskKey[Unit]("zip", "Creates a deployable artifact.")
-    def zipTask = {
-      (fullClasspath in Runtime, artifactPath in zip, artifactPath in (Compile, Keys.packageBin), streams) map {
-        (classpath, artifactPath, coreJarPath, streams) =>
-          val libs = (classpath.files +++ coreJarPath).get x flatRebase("lib")
-
-          sbt.IO.zip(libs, artifactPath)
-          streams.log.info("Created Zip:" + artifactPath)
-      }
-    }
-    
-    // --- tests-zip task
-    lazy val testsZip = TaskKey[Unit]("tests-zip", "Creates an artifact which contains integration and functional tests.")
-    def testsZipTask = {
-      (fullClasspath in Shared, artifactPath in testsZip, packageBin in Shared, packageBin in IntegrationTests, packageBin in FunctionalTests, packageBin in SystemTests, streams) map {
-        (classpath, artifactPath, sharedJarPath, integrationJarPath, functionalJarPath, systemJarPath, streams) =>
-          val libs = (classpath.files +++ sharedJarPath +++ integrationJarPath +++ functionalJarPath +++ systemJarPath).get x flatRebase("lib")
-
-          sbt.IO.zip(libs, artifactPath)
-          streams.log.info("Created Tests Zip:" + artifactPath)
-      }
-    }
-    
-        
-    
     Seq(
       scalaVersion := "2.10.2",
-      libraryDependencies := Seq("org.scalatest" % "scalatest_2.10" % "1.9.2" % "shared")      
-    ) ++
-    sharedSettings ++ unitTestSettings ++ integrationTestSettings ++ functionalTestSettings ++ systemTestSettings
-    zipTaskSettings(zip, zipTask, zipDependencies) ++
-    zipTaskSettings(testsZip, testsZipTask, testZipDependencies, "-tests")    
+      libraryDependencies := Seq("org.scalatest" % "scalatest_2.10" % "1.9.2" % "unit, integration, functional, shared")      
+    ) ++ sharedSettings ++ unitTestSettings ++ integrationTestSettings ++ functionalTestSettings ++ systemTestSettings
   }
 
   lazy val Shared = config("shared") extend(Runtime) // FIXME could we rename this to test?
@@ -94,14 +44,13 @@ object BuildSettings {
     (classDirectory in testConfiguration <<= classDirectory in Test) ++    
     (sources in testConfiguration ~= { _ filter { shouldInclude(_, testType) } } ) ++
     (testOptions in testConfiguration += Tests.Argument("-oDF")) ++
-    (artifactName in testConfiguration := { (scalaversion, moduleId, artifact) => "%s_%s-%s-%s.jar" format (moduleId.name, scalaversion.binary, buildVersion, testType) } ) ++
-    (mappings in (testConfiguration, Keys.packageBin) ~= { (ms: Seq[(File,String)]) => ms.filter { case (file, path) =>{ file.getName.endsWith(".class")==false || file.getAbsolutePath.contains(testType) } } })    
+    (mappings in (testConfiguration, Keys.packageBin) ~= { _.filter { case (file, path) => { file.getAbsolutePath.contains(testType) || !file.getName.endsWith(".class") } } } )    
   }
     
   private def shouldInclude(testFile: File, testType: String) = {
     testFile.toURI.toString.contains("/" + testType + "/")
   }  
-    
+  
 }
 
 object CasperBuild extends Build {
@@ -116,4 +65,3 @@ object CasperBuild extends Build {
     .configs(SystemTests)
     .settings(buildSettings : _*)
 }
-
