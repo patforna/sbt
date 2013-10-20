be8e8a4e2dbb3ac656595a1a1a89b05f55d2334a
Patric Fornasier <patric.fornasier@gmail.com>
2013-10-20 11:53:53 +0100

    pat: tidy up

--- project/Build.scala
+++ project/Build.scala
@@ -20,37 +20,30 @@ object BuildSettings {
       scalaVersion := "2.10.2",
       libraryDependencies := Seq("org.scalatest" % "scalatest_2.10" % "1.9.2" % "unit, integration, functional, shared")      
     ) ++ 
-    sharedSettings ++ unitTestSettings ++ integrationTestSettings ++ functionalTestSettings ++ systemTestSettings ++
+    testSettings ++
     zipSettings
   }
 
-  lazy val Shared = config("shared") extend(Runtime) // FIXME could we rename this to test?
-  lazy val sharedSettings = createTestSettings("shared", Shared, Defaults.configSettings)
-
-  lazy val UnitTests = config("unit") extend(Shared)
-  lazy val unitTestSettings = createTestSettings("unit", UnitTests)
-
+  lazy val Shared = config("shared") extend(Runtime)
+  lazy val UnitTests = config("unit") extend(Shared)  
   lazy val IntegrationTests = config("integration") extend(Shared)
-  lazy val integrationTestSettings = createTestSettings("integration", IntegrationTests)
-
   lazy val FunctionalTests = config("functional") extend(Shared)
-  lazy val functionalTestSettings = createTestSettings("functional", FunctionalTests)
-  
   lazy val SystemTests = config("system") extend(Shared)
-  lazy val systemTestSettings = createTestSettings("system", SystemTests)  
 
-  private def createTestSettings(testType: String, testConfiguration: Configuration, settings: Seq[Setting[_]] = Defaults.testSettings) = {
-    inConfig(testConfiguration)(settings) ++
-    ((fork in testConfiguration) := !debugging) ++    
-    (sourceDirectory in testConfiguration <<= sourceDirectory in Test) ++
-    (classDirectory in testConfiguration <<= classDirectory in Test) ++    
-    (sources in testConfiguration ~= { _ filter { shouldInclude(_, testType) } } ) ++
-    (testOptions in testConfiguration += Tests.Argument("-oDF")) ++
-    (mappings in (testConfiguration, Keys.packageBin) ~= { _.filter { case (file, path) => { file.getAbsolutePath.contains(testType) || !file.getName.endsWith(".class") } } } )    
+  lazy val testSettings = configure(Shared, Defaults.configSettings) ++ configure(UnitTests) ++ configure(IntegrationTests) ++ configure(FunctionalTests) ++ configure(SystemTests)
+
+  private def configure(config: Configuration, settings: Seq[Setting[_]] = Defaults.testSettings) = {
+    inConfig(config)(settings) ++
+    (fork in config := !debugging) ++    
+    (sourceDirectory in config <<= sourceDirectory in Test) ++
+    (classDirectory in config <<= classDirectory in Test) ++    
+    (sources in config ~= { _ filter { shouldInclude(_, config.name) } } ) ++
+    (testOptions in config += Tests.Argument("-oDF")) ++
+    (mappings in (config, Keys.packageBin) ~= { _.filter { case (file, path) => { file.getAbsolutePath.contains(config.name) || !file.getName.endsWith(".class") } } } )    
   }
     
-  private def shouldInclude(testFile: File, testType: String) = {
-    testFile.toURI.toString.contains("/" + testType + "/")
+  private def shouldInclude(file: File, testType: String) = {
+    file.toURI.toString.contains("/" + testType + "/")
   }  
   
   
